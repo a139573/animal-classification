76aca76e156b94f717bdc64995d1ea8603ec66dd
Andres Malon <andresmalon@gmail.com>
2025-11-05 13:40:51 +0100

    We update the init from the folder my_projects/scripts/__init__.py

---
+++ my_projects/scripts/__init__.py
@@ -0,0 +1,18 @@
+"""
+Scripts Package (my_projects.scripts)
+=====================================
+
+This package contains auxiliary scripts and utilities for the project,
+such as launching web applications or batch processes.
+
+Available Submodules:
+
+* **dashboard**: Contains the code for the interactive dashboard.
+"""
+
+# --- Recommended: Define __all__ ---
+# This tells pdoc which modules from 'scripts' to display
+
+__all__ = [
+    "dashboard"
+]
