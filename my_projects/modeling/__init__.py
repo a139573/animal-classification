d5a4386e2f66e57cdd3885e8b06c8a97d07b493b
Andres Malon <andresmalon@gmail.com>
2025-11-06 23:55:57 +0100

    We update de init files from the folder of my_modeling

---
+++ my_projects/modeling/__init__.py
@@ -0,0 +1,25 @@
+"""
+Modeling Package (my_projects.modeling)
+=======================================
+
+This package contains all modules related to Machine Learning
+for the project.
+
+This includes the model architecture definitions, the training 
+process, and functions for performing inference.
+
+Submodules:
+
+* **inference**: Functions for loading a trained model and 
+                 making predictions on new data.
+* **train**: Contains the pipeline and loops for training
+             and evaluating the models.
+"""
+
+# --- Recommended: Define __all__ ---
+# This tells pdoc which modules to document
+
+__all__ = [
+    "inference",
+    "train"
+]
